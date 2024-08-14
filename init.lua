ccce1dd07edacbf5d63df98170a2891529023c75
RSYashwanth <yashwanthrs1104@gmail.com>
2024-08-13 20:30:54 -0500

    updated lua

--- init.lua
+++ init.lua
@@ -143,7 +143,7 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = "   ", trail = "·" }
+vim.opt.listchars = { tab = "| ", trail = "·" }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
